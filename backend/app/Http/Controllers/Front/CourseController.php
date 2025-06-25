508f7964e3def87b0c098c18e38a3da3d1aeb7ff
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2025-06-25 20:59:01 +0600

    couser controller create

---
+++ backend/app/Http/Controllers/Front/CourseController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function index(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+}
