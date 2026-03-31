39ba130d39e9bcc22028dc5e17961a9ec7ebbf13
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-03-31 22:21:06 +0600

    update

--- frontend/src/components/pages/account/courses/CreateCourse.jsx
+++ frontend/src/components/pages/account/courses/CreateCourse.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import Layout from "../../../commom/Layout";
+import UserSidebar from "../../../commom/UserSidebar";
+import { Link } from "react-router-dom";
 
 const CreateCourse = () => {
   return (
     <Layout>
-      <h1>Create Course</h1>
+      <section className="section-4">
+        <div className="container pb-5 pt-3">
+          <nav aria-label="breadcrumb">
+            <ol className="breadcrumb">
+              <li className="breadcrumb-item">
+                <Link to="/account">Account</Link>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                Dashboard
+              </li>
+            </ol>
+          </nav>
+          <div className="row">
+            <div className="col-md-12 mt-5 mb-3">
+              <div className="d-flex justify-content-between">
+                <h2 className="h4 mb-0 pb-0">Create Course</h2>
+              </div>
+            </div>
+            <div className="col-lg-3 account-sidebar">
+              <UserSidebar />
+            </div>
+            <div className="col-lg-9">
+              <div className="row"></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
