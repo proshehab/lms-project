d8b64e14516fc8f4f9088c8aa35b9696efc8aa56
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-03-31 20:42:28 +0600

    update

---
+++ frontend/src/components/pages/account/courses/CreateCourse.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../../commom/Layout";
+
+const CreateCourse = () => {
+  return (
+    <Layout>
+      <h1>Create Course</h1>
+    </Layout>
+  );
+};
+
+export default CreateCourse;
